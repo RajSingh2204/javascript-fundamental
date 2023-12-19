94ffe3cddcc01ff55ad7a99b90702a4e2dd799a7
RajSingh2204 <rs13092204@gmail.com>
2023-12-19 16:52:02 +0530

    javascript fundamental

---
+++ first.js
@@ -0,0 +1,14 @@
+// for(let i=0;i<8;i++)
+// {
+    // console.log(`Raj-day ${i}`)
+// }
+//let game=["football","cricket","boxing","tabletennis"]
+//for(let i=0;i<4;i++)
+//{
+//console.log(`Raj = Day ${i+1}> ${game[i]}`)    
+//}
+let game = ["kela","banana","lichi","amrud","orange"];
+for(let i=0;i<5;i++)
+{
+    console.log(`${game[i]}`)
+}
